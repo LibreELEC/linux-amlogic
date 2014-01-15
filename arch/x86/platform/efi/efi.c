0088c039a6e3ff95dc924da5b123f576259f6b11
Matt Fleming <matt.fleming@intel.com>
2014-01-15 13:36:33 +0000

    efi: Set feature flags inside feature init functions
    It makes more sense to set the feature flag in the success path of the
    detection function than it does to rely on the caller doing it. Apart
    from it being more logical to group the code and data together, it sets
    a much better example for new EFI architectures.
    
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>
    (cherry picked from commit 0f8093a92da77afe24fd258a5a1037bb2b97a870)
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/x86/platform/efi/efi.c
+++ arch/x86/platform/efi/efi.c
@@ -589,6 +589,8 @@ static int __init efi_systab_init(void *phys)
 		       efi.systab->hdr.revision >> 16,
 		       efi.systab->hdr.revision & 0xffff);
 
+	set_bit(EFI_SYSTEM_TABLES, &efi.flags);
+
 	return 0;
 }
 
@@ -624,6 +626,8 @@ static int __init efi_runtime_init(void)
 	efi.get_time = phys_efi_get_time;
 	early_iounmap(runtime, sizeof(efi_runtime_services_t));
 
+	set_bit(EFI_RUNTIME_SERVICES, &efi.flags);
+
 	return 0;
 }
 
@@ -640,6 +644,8 @@ static int __init efi_memmap_init(void)
 
 	if (add_efi_memmap)
 		do_add_efi_memmap();
+
+	set_bit(EFI_MEMMAP, &efi.flags);
 
 	return 0;
 }
@@ -751,8 +757,6 @@ void __init efi_init(void)
 	if (efi_config_init(arch_tables))
 		return;
 
-	set_bit(EFI_CONFIG_TABLES, &efi.flags);
-
 	/*
 	 * Note: We currently don't support runtime services on an EFI
 	 * that doesn't match the kernel 32/64-bit mode.
@@ -763,7 +767,6 @@ void __init efi_init(void)
 	else {
 		if (disable_runtime || efi_runtime_init())
 			return;
-		set_bit(EFI_RUNTIME_SERVICES, &efi.flags);
 	}
 	if (efi_memmap_init())
 		return;
@@ -1206,7 +1209,7 @@ static int __init parse_efi_cmdline(char *str)
 		str++;
 
 	if (!strncmp(str, "old_map", 7))
-		set_bit(EFI_OLD_MEMMAP, &x86_efi_facility);
+		set_bit(EFI_OLD_MEMMAP, &efi.flags);
 
 	return 0;
 }
