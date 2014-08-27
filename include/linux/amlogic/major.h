b026b069db63f29e76f2a3a7987e6b552a82d859
xu xing <xing.xu@amlogic.com>
2014-08-27 11:34:25 +0000

    PD #96296: change reserve char id > 255
    Change-Id: Iaf4f3bed218b3df40ac33ce92e96b0110648217b

--- include/linux/amlogic/major.h
+++ include/linux/amlogic/major.h
@@ -1,21 +1,22 @@
 #ifndef _LINUX_MAJOR_H
 #define _LINUX_MAJOR_H
-
+#include <linux/fs.h>
 /* Amlogic extension */
-#define HDMI_MAJOR			231
-#define AUDIODSP_MAJOR		232
-#define AMSTREAM_MAJOR		233
-#define UIO_MAJOR			234
-#define USB_DEV_EP_MAJOR	235
-#define TV_CONF_MAJOR		236
-#define HIDRAW_MAJOR		237
-#define HWJPEGDEC_MAJOR		238
-#define AMVIDEO_MAJOR		239
-#define AML_DEMOD_MAJOR		242
-#define AMAUDIO_MAJOR		243
-#define AMVIDEO2_MAJOR		245
-#define TV2_CONF_MAJOR		246
-#define BLOCK_EXT_MAJOR		259
-#define SCSI_OSD_MAJOR		260	/* open-osd's OSD scsi device */
+#define AML_BASE			CHRDEV_MAJOR_HASH_SIZE
+#define HDMI_MAJOR			1+AML_BASE
+#define AUDIODSP_MAJOR		2+AML_BASE
+#define AMSTREAM_MAJOR		3+AML_BASE
+#define UIO_MAJOR			4+AML_BASE
+#define USB_DEV_EP_MAJOR	5+AML_BASE
+#define TV_CONF_MAJOR		6+AML_BASE
+#define HIDRAW_MAJOR		7+AML_BASE
+#define HWJPEGDEC_MAJOR		8+AML_BASE
+#define AMVIDEO_MAJOR		9+AML_BASE
+#define AML_DEMOD_MAJOR		10+AML_BASE
+#define AMAUDIO_MAJOR		11+AML_BASE
+#define AMVIDEO2_MAJOR		12+AML_BASE
+#define TV2_CONF_MAJOR		13+AML_BASE
+#define BLOCK_EXT_MAJOR		14+AML_BASE
+#define SCSI_OSD_MAJOR		15+AML_BASE	/* open-osd's OSD scsi device */
 
 #endif
