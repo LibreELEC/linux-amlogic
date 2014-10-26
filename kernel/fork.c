537d5f21f2cedaa8e77a5f170c55fb0fac8f2e56
Alex Deryskyba <alex@codesnake.com>
2014-10-26 21:33:37 +0200

    Fix undefined reference to kernel_thread errors when building some drivers as loadable modules

--- kernel/fork.c
+++ kernel/fork.c
@@ -1675,6 +1675,7 @@ pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags)
 	return do_fork(flags|CLONE_VM|CLONE_UNTRACED, (unsigned long)fn,
 		(unsigned long)arg, NULL, NULL);
 }
+EXPORT_SYMBOL(kernel_thread);
 
 #ifdef __ARCH_WANT_SYS_FORK
 SYSCALL_DEFINE0(fork)
