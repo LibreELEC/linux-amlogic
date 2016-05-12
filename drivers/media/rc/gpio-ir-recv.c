ada059eaee7eb8ee2c1169d1bab93ffe3ba5d459
Joy Cho <joy.cho@hardkernel.com>
2016-05-12 10:43:39 +0900

    media/rc: Add new IR receiver to specify gpio number for IR sensor
    This driver helps to register the device of GPIO based IR receiver, "gpio-ir-recv"
    with the gpio number and pulse trigger when driver is loading. For example,
    
    	# modprobe gpio-ir-recv
    	# modprobe gpioplug-ir-recv gpio_nr=249 active_low=1
    
    Change-Id: I5c16a5b108fcd34d90885d37345c84aa0ac5cf9c
    (cherry picked from commit 4aed4fc0909fd8bf9c6f7a73574e1c57fbc53db4)

--- drivers/media/rc/gpio-ir-recv.c
+++ drivers/media/rc/gpio-ir-recv.c
@@ -23,8 +23,6 @@
 #include <media/rc-core.h>
 #include <media/gpio-ir-recv.h>
 
-#define GPIO_IR_DRIVER_NAME	"gpio-rc-recv"
-#define GPIO_IR_DEVICE_NAME	"gpio_ir_recv"
 
 struct gpio_rc_dev {
 	struct rc_dev *rcdev;
