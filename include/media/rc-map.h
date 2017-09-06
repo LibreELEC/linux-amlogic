04e169713ed333d6e0af0176651e3e870bd6a7e7
Jonas Karlman <jonas@kwiboo.se>
2017-09-06 18:39:09 +0200

    rc/keymaps: add keytable for ODROID IR Remote Controller

--- include/media/rc-map.h
+++ include/media/rc-map.h
@@ -157,6 +157,7 @@ void rc_map_init(void);
 #define RC_MAP_NEC_TERRATEC_CINERGY_XS   "rc-nec-terratec-cinergy-xs"
 #define RC_MAP_NORWOOD                   "rc-norwood"
 #define RC_MAP_NPGTECH                   "rc-npgtech"
+#define RC_MAP_ODROID                    "rc-odroid"
 #define RC_MAP_PCTV_SEDNA                "rc-pctv-sedna"
 #define RC_MAP_PINNACLE_COLOR            "rc-pinnacle-color"
 #define RC_MAP_PINNACLE_GREY             "rc-pinnacle-grey"
